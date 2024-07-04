700fa14d1edd9c99d3d9f6e0af2fb2a59f806edf
Kreoin <steamuser85@yandex.ru>
2024-07-04 22:29:15 +0300

    modify RequiredCheck for String

--- app/src/main/java/hexlet/code/checks/RequiredCheck.java
+++ app/src/main/java/hexlet/code/checks/RequiredCheck.java
@@ -3,6 +3,9 @@ package hexlet.code.checks;
 public class RequiredCheck<T> implements Check<T> {
     @Override
     public boolean validate(T content) {
+        if (content == "") {
+            return false;
+        }
         return content != null;
     }
 
