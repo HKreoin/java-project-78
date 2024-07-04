75ab6351c06d90de1a74eb46e38da2527486aef1
Kreoin <steamuser85@yandex.ru>
2024-07-04 22:17:26 +0300

    refactor SizeOfCheck

--- app/src/main/java/hexlet/code/checks/SizeOfCheck.java
+++ app/src/main/java/hexlet/code/checks/SizeOfCheck.java
@@ -2,7 +2,7 @@ package hexlet.code.checks;
 
 import java.util.Map;
 
-public class SizeOfCheck implements Check<Map<?, ?>> {
+public class SizeOfCheck<T> implements Check<Map<String, T>> {
     private final int size;
 
     public SizeOfCheck(int size) {
@@ -10,7 +10,7 @@ public class SizeOfCheck implements Check<Map<?, ?>> {
     }
 
     @Override
-    public boolean validate(Map<?, ?> content) {
+    public boolean validate(Map<String, T> content) {
         return content != null && content.size() == size;
     }
 }
