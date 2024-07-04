af6978ee455a45cfdddfb2e90f7df0c5e9f4724d
Kreoin <steamuser85@yandex.ru>
2024-07-04 23:16:20 +0300

    sizeofCheck

---
+++ app/src/main/java/hexlet/code/checks/SizeofCheck.java
@@ -0,0 +1,16 @@
+package hexlet.code.checks;
+
+import java.util.Map;
+
+public class SizeofCheck<T> implements Check<Map<String, T>> {
+    private final int size;
+
+    public SizeofCheck(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean validate(Map<String, T> content) {
+        return content != null && content.size() == size;
+    }
+}
